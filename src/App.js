87df953152c2b4b35668eea96f896546e05752ed
hroulston <h_roulston@live.com>
2021-08-23 16:01:51 -0700

    create routes for components, create file with mock turtles, import turtles into app.js, route works.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,47 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import TurtleEdit from './pages/TurtleEdit';
+import TurtleIndex from './pages/TurtleIndex';
+import TurtleNew from './pages/TurtleNew';
+import TurtleShow from './pages/TurtleShow';
+import NotFound from './pages/NotFound';
+import turtles from './mockTurtle.js'
+
 
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = {
+      turtles: turtles
+    }
+  }
+  render(){
+    console.log(this.state.turtles)
     return (
-      <div>
-        
-      </div>
+      <Router>
+        <Header />
+
+        <Switch>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/turtleedit" component={TurtleEdit} />
+          <Route path="/turtleindex" component={TurtleIndex} />
+          <Route path="/turtlenew" component={TurtleNew} />
+          <Route path="/turtleshow" component={TurtleShow} />
+          <Route component={NotFound} />
+
+        </Switch>
+
+        <Footer />
+
+      </Router>
     );
   }
 }
 
 export default App;
-
---
+++ src/mockTurtle.js
@@ -0,0 +1,39 @@
+let turtles = [
+  {
+    id: 1,
+    name: "Crush",
+    age: 100,
+    enjoys: "swimming and chilling in the ocean"
+  },
+  {
+    id: 2,
+    name: "SuperHero",
+    age: 15,
+    enjoys: "dandelions, clover and sunshine"
+  },
+  {
+    id: 3,
+    name: "Kappa",
+    age: 150,
+    enjoys: "getting all the attention as half turtle half human"
+  },
+  {
+    id: 4,
+    name: "Alfie",
+    age: 78,
+    enjoys: "Quite the romantic, loves big cities"
+  },
+  {
+    id: 5,
+    name: "Morla",
+    age: 35,
+    enjoys: "Enormous and wise. Loves swamps"
+  },
+  {
+    id: 6,
+    name: "Yertle",
+    age: 120,
+    enjoys: "King of the pond, that is magnificent, beautiful, and everything else you can imagine."
+  }
+]
+export default turtles
